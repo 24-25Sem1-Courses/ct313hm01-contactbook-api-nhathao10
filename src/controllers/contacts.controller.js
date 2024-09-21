3da3e60ca0b99ae84db45d8107e9ccf0bf4b5841
Hao <lamnhuthao123@gmail.com>
2024-09-21 16:45:03 +0700

    Implement handlers

--- src/controllers/contacts.controller.js
+++ src/controllers/contacts.controller.js
@@ -1,40 +1,138 @@
-const JSend = require('../jsend');
-function createContact(req, res) {
-return res.status(201).json(JSend.success({ contact: {} }));
-}
-function getContactsByFilter(req, res) {
-const filters = [];
-const { favorite, name } = req.query;
-if (favorite !== undefined) {
-filters.push(`favorite=${favorite}`);
-}
-if (name) {
-    filters.push(`name=${name}`);
-    }
-    console.log(filters.join('&'));
-    return res.json(
-    JSend.success({
-    contacts: [],
-    })
+const contactsService = require("../services/contacts.service");
+const ApiError = require("../api-error");
+const JSend = require("../jsend");
+
+async function createContact(req, res, next) {
+  if (!req.body?.name || typeof req.body.name !== "string") {
+    return next(new ApiError(400, "Name should be a non-empty string"));
+  }
+
+  try {
+    const contact = await contactsService.createContact({
+      ...req.body,
+      avatar: req.file ? `/public/uploads/${req.file.filename}` : null,
+    });
+    return res
+      .status(201)
+      .set({
+        Location: `${req.baseUrl}/${contact.id}`,
+      })
+      .json(
+        JSend.success({
+          contact,
+        })
+      );
+  } catch (error) {
+    console.log(error);
+    return next(
+      new ApiError(500, "An error occurred while creating the contact")
     );
+  }
+}
+async function getContactsByFilter(req, res, next) {
+  let result = {
+    contacts: [],
+    metadata: {
+      totalRecords: 0,
+      firstPage: 1,
+      lastPage: 1,
+      page: 1,
+      limit: 5,
+    },
+  };
+
+  try {
+    result = await contactsService.getManyContacts(req.query);
+  } catch (error) {
+    return next(
+      new ApiError(500, "An error occured while retrieving contacts")
+    );
+  }
+
+  return res.json(
+    JSend.success({
+      contacts: result.contacts,
+      metadata: result.metadata,
+    })
+  );
+}
+
+async function getContact(req, res, next) {
+  const { id } = req.params;
+
+  try {
+    const contact = await contactsService.getContactById(id);
+    if (!contact) {
+      return next(new ApiError(404, "Contact not found"));
     }
-    function getContact(req, res) {
-    return res.json(JSend.success({ contact: {} }));
+    return res.json(JSend.success({ contact }));
+  } catch (error) {
+    console.log(error);
+    return next(new ApiError(500, `Error retrieving contact with id=${id}`));
+  }
+}
+
+async function updateContact(req, res, next) {
+  if (Object.keys(req.body).length === 0 && !req.file) {
+    return next(new ApiError(400, "Data to update can not be empty"));
+  }
+
+  const { id } = req.params;
+
+  try {
+    const updated = await contactsService.updateContact(id, {
+      ...req.body,
+      avatar: req.file ? `/public/uploads/${req.file.filename}` : null,
+    });
+
+    if (!updated) {
+      return next(new ApiError(404, "Contact not found"));
     }
-    function updateContact(req, res) {
-    return res.json(JSend.success({ contact: {} }));
+
+    return res.json(
+      JSend.success({
+        contact: updated,
+      })
+    );
+  } catch (error) {
+    console.error(error);
+    return next(new ApiError(500, `Error updating contact with id=${id}`));
+  }
+}
+
+async function deleteContact(req, res, next) {
+  const { id } = req.params;
+
+  try {
+    const deleted = await contactsService.deleteContact(id);
+    if (!deleted) {
+      return next(new ApiError(404, "Contact not found"));
     }
-    function deleteContact(req, res) {
     return res.json(JSend.success());
-    }
-    function deleteAllContacts(req, res) {
+  } catch (error) {
+    console.log(error);
+    return next(new ApiError(500, `Could not delete contact with id=${id}`));
+  }
+}
+
+async function deleteAllContacts(req, res, next) {
+  try {
+    await contactsService.deleteAllContacts();
+
     return res.json(JSend.success());
-    }
-    module.exports = {
-    getContactsByFilter,
-    deleteAllContacts,
-    getContact,
-    createContact,
-    updateContact,
-    deleteContact,
-    };
+  } catch (error) {
+    console.log(error);
+    return next(
+      new ApiError(500, "An error occurred while removing all contacts")
+    );
+  }
+}
+
+module.exports = {
+  getContactsByFilter,
+  deleteAllContacts,
+  getContact,
+  createContact,
+  updateContact,
+  deleteContact,
+};
--- src/routes/contacts.router.js
+++ src/routes/contacts.router.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const contactsController = require("../controllers/contacts.controller");
 const { methodNotAllowed } = require("../controllers/errors.controller");
+const avatarUpload = require("../middlewares/avatar-upload.middleware");
 
 const router = express.Router();
-
 module.exports.setup = (app) => {
   app.use("/api/v1/contacts", router);
   /**
@@ -23,6 +23,8 @@ module.exports.setup = (app) => {
    *         schema:
    *           type: string
    *         description: Filter by contact name
+   *       - $ref: '#/components/parameters/limitParam'
+   *       - $ref: '#/components/parameters/pageParam'
    *     tags:
    *       - contacts
    *     responses:
@@ -44,138 +46,144 @@ module.exports.setup = (app) => {
    *                       type: array
    *                       items:
    *                         $ref: '#/components/schemas/Contact'
+   *                     metadata:
+   *                       $ref: '#/components/schemas/PaginationMetadata'
    */
 
   router.get("/", contactsController.getContactsByFilter);
+
   /**
    * @swagger
    * /api/v1/contacts:
-   *  post:
-   *    summary: Create a new contact
-   *    description: Create a new contact
-   *    requestBody:
-   *      required: true
-   *      content:
-   *        multipart/from-date:
-   *          schema:
-   *            $ref: '#/components/schemas/Contact'
-   *
-   *    tags:
-   *      - contacts
-   *    responses:
-   *      201:
-   *        description: A new contact
-   *        content:
-   *          application/json:
-   *            schema:
-   *              type: object
-   *              properties:
-   *                status:
-   *                  type: string
-   *                  description: The response status
-   *                  enum: [success]
-   *                data:
-   *                  type: object
-   *                  properties:
-   *                    contact:
-   *                      $ref: '#/components/schemas/Contact'
+   *   post:
+   *     summary: Create a new contact
+   *     description: Create a new contact
+   *     requestBody:
+   *       required: true
+   *       content:
+   *         multipart/form-data:
+   *           schema:
+   *             $ref: '#/components/schemas/Contact'
+   *     tags:
+   *       - contacts
+   *     responses:
+   *       201:
+   *         description: A new contact
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: object
+   *               properties:
+   *                 status:
+   *                   type: string
+   *                   description: The response status
+   *                   enum: [success]
+   *                 data:
+   *                   type: object
+   *                   properties:
+   *                     contact:
+   *                       $ref: '#/components/schemas/Contact'
    */
+  router.post("/", avatarUpload, contactsController.createContact);
 
-  router.post("/", contactsController.createContact);
   /**
    * @swagger
-   *  /api/v1/contacts:
-   *    delete:
-   *      summary: Delete all contacts
-   *      description: Delete all contacts
-   *      tags:
-   *        - contacts
-   *      responses:
-   *        200:
-   *          description: All contacts delated
-   *          $ref: '#/components/responses/200NoData'
+   * /api/v1/contacts:
+   *   delete:
+   *     summary: Delete all contacts
+   *     description: Delete all contacts
+   *     tags:
+   *       - contacts
+   *     responses:
+   *       200:
+   *         description: All contacts deleted
+   *         $ref: '#/components/responses/200NoData'
    */
 
   router.delete("/", contactsController.deleteAllContacts);
   router.all("/", methodNotAllowed);
+
   /**
    * @swagger
-   *  /api/v1/contacts/{id}:
-   *    get:
-   *      summary: Get contact by ID
-   *      description: Get contact by ID
-   *      parameters:
-   *        - $ref: '#/components/parameters/contactIdParam'
-   *      tags:
-   *        - contacts
-   *      responses:
-   *        200:
-   *          description: A contact
-   *          content:
-   *            application/json:
-   *              schema:
-   *                type: object
-   *                properties:
-   *                  status:
-   *                    type: string
-   *                    description: The response status
-   *                    enum: [success]
-   *                  data:
-   *                    type: object
-   *                    properties:
-   *                      contact:
-   *                        $ref: '#/components/schemas/Contact'
+   * /api/v1/contacts/{id}:
+   *   get:
+   *     summary: Get contact by ID
+   *     description: Get contact by ID
+   *     parameters:
+   *       - $ref: '#/components/parameters/contactIdParam'
+   *     tags:
+   *       - contacts
+   *     responses:
+   *       '200':
+   *         description: A contact
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: object
+   *               properties:
+   *                 status:
+   *                   type: string
+   *                   description: The response status
+   *                   enum:
+   *                     - success
+   *                 data:
+   *                   type: object
+   *                   properties:
+   *                     contact:
+   *                       $ref: '#/components/schemas/Contact'
    */
   router.get("/:id", contactsController.getContact);
+
   /**
    * @swagger
-   *  /api/v1/contacts/{id}:
-   *    put:
-   *      summary: Update contact by ID
-   *      description: Update contact by ID
-   *      parameters:
-   *        - $ref: '#/components/parameters/contactIdParam'
-   *      requestBody:
-   *        required: true
-   *        content:
-   *          multipart/from-data:
-   *            schema:
-   *              $ref: '#/components/schemas/Contact'
-   *      tags:
-   *        - contacts
-   *      responses:
-   *        200:
-   *          description: An updated contact
-   *          content:
-   *            application/json:
-   *              schema:
-   *                type: object
-   *                properties:
-   *                  status:
-   *                    type: string
-   *                    description: The response status
-   *                    enum: [success]
-   *                  data:
-   *                    type: object
-   *                    properties:
-   *                      contact:
-   *                        $ref: '#/components/schemas/Contact'
+   * /api/v1/contacts/{id}:
+   *   put:
+   *     summary: Update contact by ID
+   *     description: Update contact by ID
+   *     parameters:
+   *       - $ref: '#/components/parameters/contactIdParam'
+   *     requestBody:
+   *       required: true
+   *       content:
+   *         multipart/form-data:
+   *           schema:
+   *             $ref: '#/components/schemas/Contact'
+   *     tags:
+   *       - contacts
+   *     responses:
+   *       '200':
+   *         description: An updated contact
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: object
+   *               properties:
+   *                 status:
+   *                   type: string
+   *                   description: The response status
+   *                   enum:
+   *                     - success
+   *                 data:
+   *                   type: object
+   *                   properties:
+   *                     contact:
+   *                       $ref: '#/components/schemas/Contact'
    */
-  router.put("/:id", contactsController.updateContact);
+  router.put("/:id", avatarUpload, contactsController.updateContact);
   /**
    * @swagger
-   *    /api/v1/contacts/{id}:
-   *      delete:
-   *        summary: Delete contact by ID
-   *        description: Delete contact by ID
-   *        parameters:
-   *          - $ref: '#/components/parameters/contactIdParam'
-   *        tags:
-   *          - contacts
-   *        responses:
-   *          200:
-   *            description: Contact deleted
-   *            $ref: '#/component/responses/200NoData'
+   * /api/v1/contacts/{id}:
+   *   delete:
+   *     summary: Delete contact by ID
+   *     description: Delete contact by ID
+   *     parameters:
+   *       - $ref: '#/components/parameters/contactIdParam'
+   *     tags:
+   *       - contacts
+   *     responses:
+   *       '200':
+   *         description: Contact deleted
+   *         $ref: '#/components/responses/200NoData'
    */
   router.delete("/:id", contactsController.deleteContact);
   router.all("/:id", methodNotAllowed);
